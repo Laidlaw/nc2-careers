8fb78ec916651b1571ba442714e93ea32f780c4a
Alan Laidlaw <alanlaidlaw@gmail.com>
2014-09-16 13:00:39 -0500

    Update 2014-09-16T18:00:39.678Z

--- README.md
+++
@@ -1,1 +0,0 @@
-## Example App for NC2
--- gulp/deploy.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var gulp = require('gulp');
-var deploy = require("gulp-gh-pages");
-
-gulp.task('deploy', function () {
-    gulp.src(["./dist/**/*","./CNAME"])
-        .pipe(deploy());
-});
